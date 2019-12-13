4d6da7f5d72d50f58d22ed29436f70d30b94a7ff
k-manalo <54648516+k-manalo@users.noreply.github.com>
2019-12-12 21:34:45 -0500

    final
    fixed images

--- N320_coursework/labs/final/js/app.js
+++ N320_coursework/labs/final/js/app.js
@@ -31,7 +31,7 @@ var app = new Vue({
         speed: -45,
         info:
           "Top military grade frame armor used by the Hominid Empire. This armor boasts the highest shield rating amongst all of the human frameworks. However, it's heavy weight makes it impractical for average situational use. Perfect for crowd control, A mech in the MK011 Cuirass will not be defeated by numbers alone.",
-        image: "../images/cuirass.png"
+        image: "images/cuirass.png"
       },
       {
         name: "BR9-Katana Body Armor",
@@ -40,7 +40,7 @@ var app = new Vue({
         speed: -30,
         info:
           "The BR9-Katana Body Armor is favored for it's versatility and quality in build. Black Sun arms dealers invented the framework in hopes of pitching it to the Hominid Empire for military use. Negotiations failed and now Black Sun supplies this framework to the Empire's adversaries.",
-        image: "../images/katana.png"
+        image: "images/katana.png"
       },
       {
         name: "Stryder Chest (2056 Edition)",
@@ -49,7 +49,7 @@ var app = new Vue({
         speed: -15,
         info:
           "The 2056 model of the Stryder Chest piece was renowned for revolutionizing the manueverability of mechs. Nearly 100 years later it is still favored by mercenaries for it's cheap price tag and abundance of after market parts. What the Stryder Chest lacks in shielding power it makes up for in speed. Martian rebels fitted their scouting Mechs with the Stryder Chest all the way up until their defeat 15 years ago.",
-        image: "../images/MK.png"
+        image: "images/MK.png"
       }
     ],
     curFrameId: 0,
@@ -60,7 +60,7 @@ var app = new Vue({
       speed: -45,
       info:
         "Top military grade frame armor used by the Hominid Empire. This armor boasts the highest shield rating amongst all of the human frameworks. However, it's heavy weight makes it impractical for average situational use. Perfect for crowd control, A mech in the MK011 Cuirass will not be defeated by numbers alone.",
-      image: "../images/cuirass.png"
+      image: "images/cuirass.png"
     },
     //HEAD
     head: [
@@ -71,7 +71,7 @@ var app = new Vue({
         speed: -15,
         info:
           "Deus-Cortex is the apex of artificial intelligence technology. The product of countless hours worked by the brightest at Titan University very few of these mech heads reside in the galaxy. A Deus-Cortex will fetch a steep price and so any mech encountered bearing the Deus-Cortex will find that their opponent will try to defeat them head in tact. However, trying to outsmart a mech with Deus-Cortex AI will prove futile.",
-        image: "../images/deus.png"
+        image: "images/deus.png"
       },
       {
         name: "Skullware IV",
@@ -80,7 +80,7 @@ var app = new Vue({
         speed: -7,
         info:
           "After the disastorous release of Skullware III, an event in which many mechs running the system were hacked by terrorist organizations and turned agianst the populus, Skull Tech took a 6 year hiatus. During this time Skull Tech focused on beating lawsuit cases and staying out of the public eye. After years of careful planning and countless credits being used to improve public relations, Skull Tech released Skullware IV. The success of Skullware IV is nothing short of a maricle. The AI exchanged many of the aggressive programs it's predecessor used for more humane ones. Skullware IV has become the favorite amongst SWAT teams for it's ability to recognize civilians in hostage situations.",
-        image: "../images/skull.png"
+        image: "images/skull.png"
       },
       {
         name: "Drone OS",
@@ -89,7 +89,7 @@ var app = new Vue({
         speed: -2,
         info:
           "It must be said that the Drone Operating System is equipped with some of the worst AI in the industry. Drone OS is the cheapest in it's class while still being functional. However, there are many situations where this AI could be useful. Fielding an army of mechs using Drone OS would be fairly in expensive and would suffice for eliminating human targets. Pirates and other marauders prefer Drone OS for it's simplicity. Needless to say Drone OS is quickly becoming obsolete but there are rumors that an updated version is in the works.",
-        image: "../images/drone.png"
+        image: "images/drone.png"
       }
     ],
     curHeadId: 0,
@@ -100,7 +100,7 @@ var app = new Vue({
       speed: -15,
       info:
         "Deus-Cortex is the apex of artificial intelligence technology. The product of countless hours worked by the brightest at Titan University very few of these mech heads reside in the galaxy. A Deus-Cortex will fetch a steep price and so any mech encountered bearing the Deus-Cortex will find that their opponent will try to defeat them head in tact. However, trying to outsmart a mech with Deus-Cortex AI will prove futile.",
-      image: "../images/deus.png"
+      image: "images/deus.png"
     },
     //ARM
     arm: [
@@ -111,8 +111,8 @@ var app = new Vue({
         speed: -12,
         info:
           "The development of a cost effective and lethal railgun has been the lifes work of many mechanists since the age of stellar colonialism began. Although still a prototype, this arm was accidently leaked into the public market and now every arms dealer has their own version of a railgun out of fear of the competition leaving them behind. The potential of railgun technology is undeniable. Even in it's prototype stage a railgun has the ability to cut an Imperial Battle Frigate in half with only one blast. Railguns are still not common place on the field but arms dealers cannot wait for the chance to prove their worth in order to attract the attention of investors.",
-        image: "../images/rail2.png",
-        image2: "../images/rail.png"
+        image: "images/rail2.png",
+        image2: "images/rail.png"
       },
       {
         name: "Outer Rim Pulse Cannon",
@@ -121,9 +121,9 @@ var app = new Vue({
         speed: -8,
         info:
           "Ancient technology that has become a staple for mech squadrons across the galaxy, nobody knows for sure where the Pulse Cannon came from or who invented it. Even the aliens that used it exclusively could not tell you how this arm came to be. All anybody knows is that the Pulse Cannon has an immense reputation for destroying mechs and thats all that really matters.",
-        image: "../images/pulse2.png",
+        image: "images/pulse2.png",
 
-        image2: "../images/pulse.png"
+        image2: "images/pulse.png"
       },
       {
         name: "AM18 Hammer MG",
@@ -132,8 +132,8 @@ var app = new Vue({
         speed: -4,
         info:
           "In an age wear projectile based firearms are losing popularity the Hammer MG seems nearly impervious to the rise and fall of trends in the mech industry. Due to its reliability and sufficient firepower the Hammer MG is perfect for recruits and other grunts that need a deadly weapon, but aren't enough of an asset to receive higher caliber.",
-        image: "../images/hammer.png",
-        image2: "../images/hammer2.png"
+        image: "images/hammer.png",
+        image2: "images/hammer2.png"
       }
     ],
     curArmId: 0,
@@ -144,8 +144,8 @@ var app = new Vue({
       speed: -12,
       info:
         "The development of a cost effective and lethal railgun has been the lifes work of many mechanists since the age of stellar colonialism began. Although still a prototype, this arm was accidently leaked into the public market and now every arms dealer has their own version of a railgun out of fear of the competition leaving them behind. The potential of railgun technology is undeniable. Even in it's prototype stage a railgun has the ability to cut an Imperial Battle Frigate in half with only one blast. Railguns are still not common place on the field but arms dealers cannot wait for the chance to prove their worth in order to attract the attention of investors.",
-      image: "../images/rail2.png",
-      image2: "../images/rail.png"
+      image: "images/rail2.png",
+      image2: "images/rail.png"
     },
     //LEG
     leg: [
@@ -155,7 +155,7 @@ var app = new Vue({
         speed: 30,
         info:
           "The Mantis Greaves received it's name from its ability to launch mechs to high altitudes. An alien invention at heart, the Mantis greaves mimic the aliens natural ability to jump to high places. The Mantis Greaves are one of the heaviest leg components on the market, but it's sophisticated thrusters allow mechs to sprint to incredible speeds and to leap over skyscrapers.",
-        image: "../images/mantis.png"
+        image: "images/mantis.png"
       },
       {
         name: "Martian Steppe Navigators",
@@ -163,7 +163,7 @@ var app = new Vue({
         speed: 21,
         info:
           "The Martian Steppe Navigators saw a lot of action in the rebellion. When the Hominid Empire's main legion landed on mars they were met by a large contingent of rebel mechs moving across the martian dunes as if it were nothing. The Martian Steppe Navigators epitomize all terrain and in 2139 it was named Mecha Aficionado's Mech Component of the Year.",
-        image: "../images/martian.png"
+        image: "images/martian.png"
       },
       {
         name: "Basic Thrusters V.2",
@@ -171,7 +171,7 @@ var app = new Vue({
         speed: 15,
         info:
           "These basic thrusters are primarily used for bootcamp training simulations and are rarely seen off a military compound. However, a few models have found their way into circulation and are now sought after by any juggernaut enthusiast who wishes to sacrifice their speed for armor/firepower.",
-        image: "../images/thruster.png"
+        image: "images/thruster.png"
       }
     ],
     curLegId: 0,
@@ -181,7 +181,7 @@ var app = new Vue({
       speed: 30,
       info:
         "The Mantis Greaves received it's name from its ability to launch mechs to high altitudes. An alien invention at heart, the Mantis greaves mimic the aliens natural ability to jump to high places. The Mantis Greaves are one of the heaviest leg components on the market, but it's sophisticated thrusters allow mechs to sprint to incredible speeds and to leap over skyscrapers.",
-      image: "../images/mantis.png"
+      image: "images/mantis.png"
     },
     fcardActive: true,
     hcardActive: true,
